2c1b43535cbe5b94bf0fe881e93085dcc32d50fe
corvinus-p97bbn <79064254+corvinus-p97bbn@users.noreply.github.com>
2021-03-28 23:39:47 +0200

    7.het

---
+++ 4.het/4.het/wwwroot/hetedik.js
@@ -0,0 +1,29 @@
+﻿var kérdések;
+
+    
+fetch('/questions.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kérdések = d;
+
+}
+window.onload = () => {
+    letöltés();
+    document.getElementById("back").onclick = () => {
+        sorszam--;
+        if (sorszam== -1)
+        {
+            megjelenites(sorszam[2]);
+        }
+        document.getElementById("next").onclick = () => {
+            sorszam++;
+            if (sorszam == 3) {
+                megjelenites(sorszam[0]);
+            }
+    }
+}
